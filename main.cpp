51e6564d74c47858112dfbfcc9a0e1c37b2edda1
Jabke <sereja.gal@yandex.ru>
2023-10-07 13:22:11 +0300

    It works
    It works for two base tests

--- main.cpp
+++ main.cpp
@@ -1,14 +1,35 @@
+/*
+Ссылка на контест: https://contest.yandex.ru/contest/53768/enter
+
+1.4 Инвертируйте значение бита в числе N по его номеру K.
+Формат входных данных. Число N, номер бита K.
+Формат выходных данных. Число с инвертированным битом в десятичном виде.
+
+in          out
+25 1        27
+25 4        9 */
+
 #include <iostream>
 
 class Solution {
  public:
- private:
+  uint InvertBit(uint original_num, uint K);  // K - bit number
 };
+
+//-----------------------------------------------------------------------------
+
+uint Solution::InvertBit(uint original_num, uint K) {
+    uint second_operand = 1;  // we need number like 00100(only one "1")
+    return original_num^(second_operand << K);
+}
+
+//-----------------------------------------------------------------------------
 
 void TestFunction() {
 }
 
 int main() {
-    std::cout << "Repo" << std::endl;
+    Solution s;
+    std::cout << s.InvertBit(25, 4) << std::endl;
     return 1;
 }
