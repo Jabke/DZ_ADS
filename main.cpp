c5eaa9236eb6dd29c51847952e2da01f57052a46
Jabke <sereja.gal@yandex.ru>
2023-10-23 01:02:40 +0300

    Exp search work
    Add custom, array, pair and pow

--- main.cpp
+++ main.cpp
@@ -29,9 +29,84 @@
 */
 
 #include <iostream>
+//---CustomPow----------------------------------------------------------------------
+
+int Pow(int base, int degree) {
+    if (degree == 0)
+        return 1;
+    int buff = base;
+    for (int i = 1; i < degree; ++i)
+        base *= buff;
+    return base;
+}
+
+//---CustomArray----------------------------------------------------------------------
+
+struct CustomArray {
+    int count_of_elements_ = 0;
+    int* array_ = nullptr;
+
+    //-------------------------------------------------------------------------
+
+    CustomArray(const CustomArray&) = delete;
+
+    //-------------------------------------------------------------------------
+
+    CustomArray(int count_of_elements) : count_of_elements_(count_of_elements) {
+        array_ = new int[count_of_elements_]{0};
+        for (int i = 0;  i < count_of_elements_; ++i)
+            std::cin >> array_[i];
+    }
+
+    //-------------------------------------------------------------------------
+
+    ~CustomArray() {
+        delete[] array_;
+    }
+
+};
+
+//-------------------------------------------------------------------------
+
+std::ostream& operator<<(std::ostream& out, const CustomArray& array) {
+    for (int i = 0; i < array.count_of_elements_; ++i)
+        out << array.array_[i] << " ";
+    return out;
+}
+
+//-----CustomPair--------------------------------------------------------------------
+
+struct CustomPair {
+    int a = 0;
+    int b = 0;
+};
+
+std::ostream& operator<<(std::ostream& out, const CustomPair& pair) {
+    out << pair.a << " " << pair.b;
+    return out;
+}
+
+//-----Solution--------------------------------------------------------------------
 
 class Solution {
  public:
+    CustomPair ExponentialSearch(const CustomArray& A, int target_number) {
+        CustomPair pair{-1, -1};  // This struct contains the indexes of the interval
+        if (A.array_[0] == target_number) {  // What if A[0] - our target?
+            return pair = {0, 0};
+        }
+        int right = 0;
+        int left = 0;
+        for (int i = 0; i < A.count_of_elements_; ++i) {
+            left = Pow(2, i);
+            right = Pow(2, i + 1);
+            if (A.array_[left] <= target_number && A.array_[right] > target_number) {
+                pair = {left, right};
+                break;
+            }
+        }
+        return pair;
+    }
  private:
 };
 
@@ -39,9 +114,14 @@ void TestFunction() {
 }
 
 int main() {
-    int argument_one = 0;
-    int argument_two = 0;
-    std::cin >> argument_one >> argument_two;
-    std::cout << "Repo" << std::endl;
+    Solution s;
+    int n = 0;
+    int m = 0;
+    std::cin >> n >> m;
+    CustomArray A(n);  // sort
+    CustomArray B(m);
+    std::cout << A << std::endl;
+    std::cout << B << std::endl;
+    std::cout << s.ExponentialSearch(A, B.array_[0]) << std::endl;
     return 0;
 }
