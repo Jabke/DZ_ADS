39124347b8707937cf56f6d117889f9d76de52d2
Jabke <sereja.gal@yandex.ru>
2023-11-01 23:40:33 +0300

    Not work
    More tests, reduce not work

--- main.cpp
+++ main.cpp
@@ -70,10 +70,18 @@ class Queue {
     if (queue_size_ == 0)
       return -1;  // Условие задачи
     int buffer = array_[head_];
-    head_++;
     queue_size_--;
-    if (queue_size_ <= (buffer_size_/3))
-       Reduce();
+    /*if (queue_size_ != 0)
+      head_++;
+    if (head_ == buffer_size_ - 1)
+      head_ = 0;*/
+    head_ = (head_ + 1)%buffer_size_;
+    if (queue_size_ == 0) {
+      head_ = 0;
+      tail_ = 0;
+    }
+    //if (queue_size_ > 3 && queue_size_ <= (buffer_size_/3))
+    // Reduce();
     return buffer;
   }
 
@@ -149,12 +157,10 @@ class Queue {
     if (queue_size_ <= (buffer_size_ / 3) && buffer_size_ / 2 > 2) {
       buffer_size_ /= 2;
       int* new_array = new int[buffer_size_]{0};
-      for (int i = 0; i < queue_size_; ++i) {
-        if ((head_ + i) < (buffer_size_ - 1)) {
-          new_array[i] = array_[head_ + i];
-        } else {
-          new_array[i] = array_[i];
-        }
+      int j = 0;
+      for (int i = head_; i != queue_size_; i = (i + 1)%buffer_size_) {
+        new_array[j] = array_[i];
+        ++j;
       }
       delete[] array_;
       head_ = 0;
@@ -195,6 +201,8 @@ class Solution {
       in >> expected_value;
       switch (method_selection) {
         case 2:
+          /*queue.OutQueue();
+          queue.OutBuffer();*/
           debug_buffer = queue.PopFront();
           if (expected_value == debug_buffer && flag) {
             flag = true;
@@ -220,7 +228,7 @@ class Solution {
 };
 
 void TestFunction() {
-  constexpr int count_of_tests = 10;
+  constexpr int count_of_tests = 20;
   std::istringstream streams_string[count_of_tests];
   streams_string[0].str("3 3 44 3 50 2 44");
   streams_string[1].str("2 2 -1 3 10");
@@ -232,10 +240,30 @@ void TestFunction() {
   streams_string[7].str("6 3 5 3 6 3 44 3 55 2 5 2 6 3 56 2 44"); // Сначала расширяемся, потом получаем нечто такое 0 * * * 0 0
   streams_string[8].str("8 3 5 3 6 3 44 3 55 2 5 2 6 3 56 2 -1");  // Расширение -> выем -> добавление -> расширение
   streams_string[9].str("9 3 5 3 6 3 44 3 55 2 5 2 6 3 56 2 -1 2 55");  // Не верно одно число в середине
+  streams_string[11].str("13 3 118 2 118 3 101 2 101 3 6 3 110 3 3 2 6 2 110 2 3 3 91 2 91 3 70"); // 7 тест контест
+  streams_string[10].str("14 3 118 3 110 3 101 3 105 3 109 3 110 3 3 2 118 2 110 2 101 2 105 2 109 2 110 2 3");
+  streams_string[12].str("15 3 118 3 110 3 101 2 118 2 110 2 101 2 -1 3 105 3 109 3 110 3 3 2 105 2 109 2 110 2 3");
+  streams_string[13].str("16 3 118 3 110 3 101 2 118 2 110 2 101 2 -1 3 105 3 109 3 110 3 3 2 105 2 109 2 110 2 3 2 -1");
+  streams_string[14].str("6 2 -1 3 1 3 2 2 1 2 2 2 -1");
+  streams_string[15].str("7 3 0 3 0 3 0 2 0 2 0 2 0 2 -1");
+  streams_string[16].str("7 3 -1 3 -1 3 -1 2 -1 2 -1 2 -1 2 -1");
+  streams_string[17].str("7 3 10 3 11 3 12 2 -1 2 11 2 12 2 -1");
+  streams_string[18].str("8 2 2 2 11 3 10 3 -1 2 10 3 10 2 -1 2 10");
+  streams_string[19].str("7 3 -2 3 -11 2 -2 2 -11 2 -10 3 -10 2 -10");
+  // streams_string[10].str("9 3 5 3 4 3 6 2 5 2 4 3 7 3 8 2 6 2 7");
 
     // Значение не совпало где-то посередине
   Solution s;
   std::ostringstream answer;
+ /* std::string buffer_string = "";
+  s.YesOrNo(answer, streams_string[0]);
+  buffer_string = answer.str();
+  answer.str("");
+  if (buffer_string == answers[0]) {
+    std::cout << "Test " << 0 << " OK" << std::endl;
+  } else {
+    std::cout << "Test " << 0 << " WA" << std::endl;
+  }*/
   std::string answers[count_of_tests] = {
       {"YES"},
       {"YES"},
@@ -246,8 +274,19 @@ void TestFunction() {
       {"YES"},
       {"YES"},
       {"NO"},
-      {"NO"}
+      {"NO"},
+      {"YES"},
+      {"YES"},
+      {"YES"},
+      {"YES"},
+      {"YES"},
+      {"YES"},
+      {"YES"},
+      {"NO"},
+      {"NO"},
+      {"NO"},
   };
+
   std::string buffer_string = "";
   for (int i = 0; i < count_of_tests; ++i) {
     s.YesOrNo(answer, streams_string[i]);
@@ -263,7 +302,7 @@ void TestFunction() {
 }
 
 int main() {
-  //TestFunction();
+  // TestFunction();
   Solution s;
   s.YesOrNo(std::cout, std::cin);
   return 0;
